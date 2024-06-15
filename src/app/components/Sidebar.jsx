56ec535c017ce36e8234e9e1894d88697b7ced44
Nikolay9812 <73443601+Nikolay9812@users.noreply.github.com>
2024-06-15 07:37:54 +0200

    Modify the sidebar component

--- src/app/components/Sidebar.jsx
+++ src/app/components/Sidebar.jsx
@@ -1,7 +1,24 @@
-import React from 'react'
+import Link from "next/link";
+import React from "react";
+import { FaXTwitter } from "react-icons/fa6";
+import { HiHome } from "react-icons/hi";
 
 export default function Sidebar() {
   return (
-    <div>Sidebar</div>
-  )
+    <div className="flex flex-col gap-4 p-3">
+      <Link href="/">
+        <FaXTwitter className="w-16 h-16 cursor-pointer p-3 hover:bg-gray-100 rounded-full transition-all duration-300" />
+      </Link>
+      <Link
+        href="/"
+        className="flex items-center p-3 gap-2 w-fit hover:bg-gray-100 rounded-full transition-all duration-300"
+      >
+        <HiHome className="w-7 h-7" />
+        <span className="font-bold hidden xl:inline">Home</span>
+      </Link>
+      <button className="bg-blue-400 hover:bg-blue-600 shadow-md transition-all duration-300 text-white rounded-full w-48 h-9 hidden xl:inline">
+        Sign in
+      </button>
+    </div>
+  );
 }
