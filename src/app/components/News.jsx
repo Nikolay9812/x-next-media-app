89788ff725de2f63ac60eae3bb558e54c6285436
Nikolay9812 <73443601+Nikolay9812@users.noreply.github.com>
2024-06-15 19:06:04 +0200

    adiin next-auth authentication

--- src/app/components/News.jsx
+++ src/app/components/News.jsx
@@ -11,14 +11,16 @@ export default function News() {
       .then((res) => res.json())
       .then((data) => {
         setNews(data.articles);
-      });
-  });
+      })
+      .catch((error) => console.error("Error fetching news:", error));
+  }, []); // Add an empty dependency array to run the effect only once
+
   return (
     <div className="text-gray-700 space-y-3 bg-gray-100 rounded-xl pt-2">
-      <h4 className="font-bold text-xl px-4">Whats Happening</h4>
+      <h4 className="font-bold text-xl px-4">What's Happening</h4>
       {news.slice(0, articleNum).map((article) => (
         <div key={article.url}>
-          <a href={article.url} target="_blank">
+          <a href={article.url} target="_blank" rel="noopener noreferrer">
             <div className="flex items-center justify-between px-4 py-2 space-x-1 hover:bg-gray-200 transition-all duration-300">
               <div className="space-y-0.5">
                 <h6 className="text-sm font-bold">{article.title}</h6>
@@ -37,7 +39,8 @@ export default function News() {
       ))}
       <button
         onClick={() => setArticleNum(articleNum + 3)}
-className="text-blue-300 hover:text-blue-400 transition duration-300 pl-4 pb-3"      >
+        className="text-blue-300 hover:text-blue-400 transition duration-300 pl-4 pb-3"
+      >
         Show More
       </button>
     </div>
--- src/app/components/Sidebar.jsx
+++ src/app/components/Sidebar.jsx
@@ -1,9 +1,13 @@
+"use client";
+
+import { signIn, signOut, useSession } from "next-auth/react";
 import Link from "next/link";
 import React from "react";
 import { FaXTwitter } from "react-icons/fa6";
 import { HiHome } from "react-icons/hi";
 
 export default function Sidebar() {
+  const { data: session } = useSession();
   return (
     <div className="flex flex-col gap-4 p-3">
       <Link href="/">
@@ -16,9 +20,21 @@ export default function Sidebar() {
         <HiHome className="w-7 h-7" />
         <span className="font-bold hidden xl:inline">Home</span>
       </Link>
-      <button className="bg-blue-400 hover:bg-blue-600 shadow-md font-semibold transition-all duration-300 text-white rounded-full w-48 h-9 hidden xl:inline">
-        Sign in
-      </button>
+      {session ? (
+        <button
+          className="bg-blue-400 hover:bg-blue-600 shadow-md font-semibold transition-all duration-300 text-white rounded-full w-48 h-9 hidden xl:inline"
+          onClick={() => signOut()}
+        >
+          Sign Out
+        </button>
+      ) : (
+        <button
+          className="bg-blue-400 hover:bg-blue-600 shadow-md font-semibold transition-all duration-300 text-white rounded-full w-48 h-9 hidden xl:inline"
+          onClick={() => signIn()}
+        >
+          Sign In
+        </button>
+      )}
     </div>
   );
 }
