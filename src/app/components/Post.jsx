a0d590fd5a865d8d46184909e8fba790d1bfde0b
Nikolay9812 <73443601+Nikolay9812@users.noreply.github.com>
2024-06-16 20:03:23 +0200

    handle post seletion

--- src/app/components/Post.jsx
+++ src/app/components/Post.jsx
@@ -6,7 +6,11 @@ import Icons from "./Icons";
 export default function Post({ post, id }) {
   return (
     <div className="flex p-3 border-b border-gray-200 hover:bg-gray-50">
-      <img src={post?.profileImg} alt="user-img" className="h-11 w-11 rounded-full mr-4" />
+      <img
+        src={post?.profileImg}
+        alt="user-img"
+        className="h-11 w-11 rounded-full mr-4"
+      />
       <div className="flex-1">
         <div className="flex items-center justify-between">
           <div className="flex items-center space-x-1 whitespace-nowrap">
@@ -14,14 +18,14 @@ export default function Post({ post, id }) {
             <span className="text-xs truncate">@{post?.username}</span>
           </div>
           <HiDotsHorizontal className="text-sm" />
-              </div>
-              <Link href={`/posts/${id}`}>
-                  <p className="text-gray-800 text-sm my-3">{ post?.text}</p>
-              </Link>
-              <Link href={`/posts/${id}`}>
-                  <img src={post?.image} className="rounded-2xl mr-2" />
-              </Link>
-              <Icons/>
+        </div>
+        <Link href={`/posts/${id}`}>
+          <p className="text-gray-800 text-sm my-3">{post?.text}</p>
+        </Link>
+        <Link href={`/posts/${id}`}>
+          <img src={post?.image} className="rounded-2xl mr-2" />
+        </Link>
+        <Icons id={id} uid={post.uid} />
       </div>
     </div>
   );
