2cc642324435138af9634e4c7e4597e9c9c5b989
Nikolay9812 <73443601+Nikolay9812@users.noreply.github.com>
2024-06-16 08:37:51 +0200

    Add Firease storage and complete file upload functionality

--- src/app/components/Input.jsx
+++ src/app/components/Input.jsx
@@ -1,24 +1,107 @@
 "use client";
 
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { useSession } from "next-auth/react";
 import { HiOutlinePhotograph } from "react-icons/hi";
+import { app } from "../firebase";
+import {
+  getStorage,
+  ref,
+  uploadBytesResumable,
+  getDownloadURL,
+} from "firebase/storage";
 
 export default function Input() {
   const { data: session } = useSession();
+  const [imgFileUrl, setImgFileUrl] = useState(null);
+  const [selectedFile, setSelectedFile] = useState(null);
+  const [imageFileUploaing, setImageFileUploading] = useState(false);
+
+  const imagePickRef = useRef(null);
+  const addImageToPost = (e) => {
+    const file = e.target.files[0];
+    if (file) {
+      setSelectedFile(file);
+      setImgFileUrl(URL.createObjectURL(file));
+    }
+  };
+
+  useEffect(() => {
+    if (selectedFile) {
+      uploadImageToStorage();
+    }
+  }, [selectedFile]);
+
+  const uploadImageToStorage = () => {
+    setImageFileUploading(true);
+    const storage = getStorage(app);
+    const fileName = new Date().getTime() + "-" + selectedFile.name;
+    const storageRef = ref(storage, fileName);
+    const uploadTask = uploadBytesResumable(storageRef, selectedFile);
+    uploadTask.on(
+      "state_changed",
+      (snapshot) => {
+        const progress =
+          (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+        console.log("Upload is" + progress + "% done");
+      },
+      (error) => {
+        console.log(error);
+        setImageFileUploading(false);
+        setImgFileUrl(null);
+        setSelectedFile(null);
+      },
+      () => {
+        getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+          setImgFileUrl(downloadURL);
+          setImageFileUploading(false);
+        });
+      }
+    );
+  };
   if (!session) return null;
-    return <div className="flex border-b border-gray-200 p-3 space-x-3 w-full">
+  return (
+    <div className="flex border-b border-gray-200 p-3 space-x-3 w-full">
       <img
-            src={session.user.image}
-            alt="user-img"
-            className="h-11 w-11 rounded-full cursor-pointer hover:brightness-95"
-        />
-        <div className="w-full divide-y divide-gray-200">
-            <textarea className="w-full border-none outline-none tracking-wide min-h-[50px] text-gray-700" name="textInput" id="textInput" placeholder="Whats happening" rows='2'></textarea>
-            <div className="flex items-center justify-between pt-2.5">
-                <HiOutlinePhotograph className="h-10 w-10 p-2 text-sky-500 hover:bg-sky-100 rounded-full cursor-pointer" />
-                <button disabled className="bg-blue-400 text-white px-4 py-1.5 rounded-full font-bold shadow-md brightness-95 disabled:opacity-50">Post</button>
-            </div>
+        src={session.user.image}
+        alt="user-img"
+        className="h-11 w-11 rounded-full cursor-pointer hover:brightness-95"
+      />
+      <div className="w-full divide-y divide-gray-200">
+        <textarea
+          className="w-full border-none outline-none tracking-wide min-h-[50px] text-gray-700"
+          name="textInput"
+          id="textInput"
+          placeholder="Whats happening"
+          rows="2"
+        ></textarea>
+        {selectedFile && (
+          <img
+            src={imgFileUrl}
+            alt="image"
+            className="w-full max-h-[250px] object-cover cursor-pointer"
+          />
+        )}
+        <div className="flex items-center justify-between pt-2.5">
+          <HiOutlinePhotograph
+            onClick={() => imagePickRef.current.click()}
+            className="h-10 w-10 p-2 text-sky-500 hover:bg-sky-100 rounded-full cursor-pointer"
+          />
+          <input
+            type="file"
+            ref={imagePickRef}
+            accept="image/*"
+            onChange={addImageToPost}
+            hidden
+          />
+          <button
+            disabled
+            className="bg-blue-400 text-white px-4 py-1.5 rounded-full font-bold shadow-md brightness-95 disabled:opacity-50"
+          >
+            Post
+          </button>
         </div>
-  </div>;
+      </div>
+    </div>
+  );
 }
---
+++ src/app/firebase.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: "x-next-79687.firebaseapp.com",
+  projectId: "x-next-79687",
+  storageBucket: "x-next-79687.appspot.com",
+  messagingSenderId: "600740940166",
+  appId: "1:600740940166:web:1a919ad57438598aea8407",
+  measurementId: "G-72XC78WYBH",
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
