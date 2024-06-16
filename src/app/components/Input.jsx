2b654ad33183b9afbad929cfe2f8d533b84d6b30
Nikolay9812 <73443601+Nikolay9812@users.noreply.github.com>
2024-06-16 09:20:59 +0200

    add post creation feature

--- src/app/components/Input.jsx
+++ src/app/components/Input.jsx
@@ -10,14 +10,25 @@ import {
   uploadBytesResumable,
   getDownloadURL,
 } from "firebase/storage";
+import {
+  Timestamp,
+  addDoc,
+  collection,
+  getFirestore,
+  serverTimestamp,
+} from "firebase/firestore";
 
 export default function Input() {
   const { data: session } = useSession();
   const [imgFileUrl, setImgFileUrl] = useState(null);
   const [selectedFile, setSelectedFile] = useState(null);
-  const [imageFileUploaing, setImageFileUploading] = useState(false);
+  const [imageFileUploading, setImageFileUploading] = useState(false);
+  const [text, setText] = useState("");
+  const [postLoading, setPostLoading] = useState(false);
 
   const imagePickRef = useRef(null);
+  const db = getFirestore(app);
+
   const addImageToPost = (e) => {
     const file = e.target.files[0];
     if (file) {
@@ -59,6 +70,24 @@ export default function Input() {
       }
     );
   };
+
+  const handleSubmit = async () => {
+    setPostLoading(true);
+    const docRef = await addDoc(collection(db, "posts"), {
+      uid: session.user.uid,
+      name: session.user.name,
+      username: session.user.username,
+      text,
+      image: imgFileUrl,
+      profileImg: session.user.image,
+      timestamp: serverTimestamp(),
+    });
+    setPostLoading(false);
+    setText("");
+    setImgFileUrl(null);
+    setSelectedFile(null);
+  };
+
   if (!session) return null;
   return (
     <div className="flex border-b border-gray-200 p-3 space-x-3 w-full">
@@ -74,12 +103,16 @@ export default function Input() {
           id="textInput"
           placeholder="Whats happening"
           rows="2"
+          value={text}
+          onChange={(e) => setText(e.target.value)}
         ></textarea>
         {selectedFile && (
           <img
             src={imgFileUrl}
             alt="image"
-            className="w-full max-h-[250px] object-cover cursor-pointer"
+            className={`w-full max-h-[250px] object-cover cursor-pointer ${
+              imageFileUploading ? "animate-pulse" : ""
+            }`}
           />
         )}
         <div className="flex items-center justify-between pt-2.5">
@@ -95,8 +128,9 @@ export default function Input() {
             hidden
           />
           <button
-            disabled
+            disabled={text.trim() === "" || postLoading || imageFileUploading}
             className="bg-blue-400 text-white px-4 py-1.5 rounded-full font-bold shadow-md brightness-95 disabled:opacity-50"
+            onClick={handleSubmit}
           >
             Post
           </button>
